c4ff7538faf06e0741b3731af20bcfd39f539ae4
Lamine DIOUBATE <domanilamine96@gmail.com>
2024-11-27 17:19:00 -0500

    user models done

--- dist/db/index.js
+++ dist/db/index.js
@@ -1,12 +1,17 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
-const variables_1 = require("#/utils/variables");
-mongoose_1.default.connect(variables_1.MONGO_URI).then(() => {
+const variables_1 = require("#/utils/variables"); ///en cas d'erreur a changer a "../utils/variables"
+mongoose_1.default
+  .connect(variables_1.MONGO_URI)
+  .then(() => {
     console.log("DB is connected!");
-}).catch((err) => {
+  })
+  .catch((err) => {
     console.log("DB connection failed!", err);
-});
+  });
---
+++ src/models/user.ts
@@ -0,0 +1,57 @@
+import { model, Model, ObjectId, Schema } from "mongoose";
+
+//creation d'interface(typescript) pour le model de l'utilisateur.
+interface UserDocuments{
+    name: string;
+    email: string;
+    password: string;
+    verified: boolean;
+    avatar?: {url: string; publicId: string}
+    tokens: string[];
+    favarites: ObjectId[];
+    followers: ObjectId[];
+    followings: ObjectId[];
+}
+
+const userSchema = new Schema<UserDocuments>({
+    name:{
+        type: String,
+        required:true,
+        trim: true,
+    },
+    email:{
+        type: String,
+        required:true,
+        trim: true,
+        unique: true,
+    },
+    password:{
+        type: String,
+        required:true,
+    },
+    avatar:{
+        type: Object,
+        url: String,
+        publicId: String,
+    },
+    verified:{
+        type: Boolean,
+       default: false,
+    },
+    favarites:[{
+        type: Schema.Types.ObjectId,
+        ref: "Audio"
+    }],
+    followers:[{
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    }],
+    followings:[{
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    }],
+    tokens: [String],
+}, {timestamps: true});
+
+export default model("User", userSchema) as Model<UserDocuments>
+

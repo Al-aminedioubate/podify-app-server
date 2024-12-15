fdb043c181b41af0df5aa4fbf8ba2b973fb6109d
Lamine DIOUBATE <domanilamine96@gmail.com>
2024-12-15 11:46:56 -0500

    refactor for email send completed

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,65 +1,17 @@
-import { RequestHandler } from "express";
-import nodemailer from "nodemailer";
-
 import { CreateUser } from "#/@types/user";
-import User from '#/models/user';
-import EmailVerificationToken from "#/models/emailVerificationToken";
-import { MAILTRAP_PASS, MAILTRAP_USER } from "#/utils/variables";
+import { RequestHandler } from "express";
+import User from "#/models/user";
 import { generateToken } from "#/utils/helper";
-import { generateTemplate } from "#/mail/template";
-
-import path from 'path';
+import { sendVerificationMail } from "#/utils/mail";
 
 export const create: RequestHandler = async (req: CreateUser, res) =>{
     const {email, password, name } = req.body;
 
-
     const user = await User.create({ name, email, password });
-    //Send email verification
-    var transport = nodemailer.createTransport({
-        host: "sandbox.smtp.mailtrap.io",
-        port: 2525,
-        auth: {
-          user: MAILTRAP_USER,
-          pass: MAILTRAP_PASS
-        }
-    });
-    
-    //creation of token 
+
+    //send verification email
     const token = generateToken();
-    const newToken = await EmailVerificationToken.create({
-        owner: user._id,
-        token: token,
-    });
+    sendVerificationMail(token, {name, email, userId: user._id.toString()})
 
-    //notre message de mailtrap
-    const welcomeMessage = `Hi ${name}, welcome to Podify! There are so much thing that we do for verified users. Use the given OTP to verify your email.`;
-
-    //ce code nous permet d'envoyer l'email de verification dans nodemailer.
-    transport.sendMail({
-        to: user.email,
-        from: "auth@myapp.com",
-        html: generateTemplate({
-            title: "Welcome to Podify",
-            message: welcomeMessage,
-            logo: "cid:logo",
-            banner: "cid:welcome",
-            link: "#",
-            btnTitle: token
-        }),
-        attachments:[
-            {
-                filename: "logo.png",
-                path: path.join(__dirname, "../mail/logo.png"),
-                cid: "logo",
-            },
-            {
-                filename: "welcome.png",
-                path: path.join(__dirname, "../mail/welcome.png"),
-                cid: "welcome",
-            },
-        ]
-    });
-
-    res.status(201).json({ user })
+    res.status(201).json({ user: {id: user._id, name, email} });        //we dont want to send the full user so sending only id, name & email.
 };
---
+++ src/utils/mail.ts
@@ -0,0 +1,69 @@
+import nodemailer from "nodemailer";
+
+import User from '#/models/user';
+import EmailVerificationToken from "#/models/emailVerificationToken";
+import { MAILTRAP_PASS, MAILTRAP_USER, VERIFICATION_EMAIL } from "#/utils/variables";
+import { generateToken } from "#/utils/helper";
+import { generateTemplate } from "#/mail/template";
+import path from "path";
+
+const generateMailTransporter = () => {
+    //Send email verification
+    const transport = nodemailer.createTransport({
+        host: "sandbox.smtp.mailtrap.io",
+        port: 2525,
+        auth: {
+        user: MAILTRAP_USER,
+        pass: MAILTRAP_PASS
+        }
+    });
+     
+    return transport
+} 
+
+interface Profile{
+    name: string;
+    email: string;
+    userId: string;
+}
+
+export const sendVerificationMail = async (token: string, profile: Profile) =>{
+    const transport = generateMailTransporter();
+    const {name, email, userId} = profile;
+
+    //creation of token 
+    await EmailVerificationToken.create({
+        owner: userId,
+        token: token,
+    });
+
+    //notre message de mailtrap
+    const welcomeMessage = `Hi ${name}, welcome to Podify! There are so much thing that we do for verified users. Use the given OTP to verify your email.`;
+
+    //ce code nous permet d'envoyer l'email de verification dans nodemailer.
+    transport.sendMail({
+        to: email,
+        from: VERIFICATION_EMAIL,
+        html: generateTemplate({
+            title: "Welcome to Podify",
+            message: welcomeMessage,
+            logo: "cid:logo",
+            banner: "cid:welcome",
+            link: "#",
+            btnTitle: token
+        }),
+        attachments:[
+            {
+                filename: "logo.png",
+                path: path.join(__dirname, "../mail/logo.png"),
+                cid: "logo",
+            },
+            {
+                filename: "welcome.png",
+                path: path.join(__dirname, "../mail/welcome.png"),
+                cid: "welcome",
+            },
+        ]
+    });
+}
+

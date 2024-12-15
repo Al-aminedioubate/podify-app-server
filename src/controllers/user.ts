26f71d973aa1d49d1cabb52b1af8cdc4b43e1c42
Lamine DIOUBATE <domanilamine96@gmail.com>
2024-12-15 10:44:36 -0500

    sending user verification mail with template done

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -6,9 +6,13 @@ import User from '#/models/user';
 import EmailVerificationToken from "#/models/emailVerificationToken";
 import { MAILTRAP_PASS, MAILTRAP_USER } from "#/utils/variables";
 import { generateToken } from "#/utils/helper";
+import { generateTemplate } from "#/mail/template";
+
+import path from 'path';
 
 export const create: RequestHandler = async (req: CreateUser, res) =>{
     const {email, password, name } = req.body;
+
 
     const user = await User.create({ name, email, password });
     //Send email verification
@@ -28,11 +32,33 @@ export const create: RequestHandler = async (req: CreateUser, res) =>{
         token: token,
     });
 
+    //notre message de mailtrap
+    const welcomeMessage = `Hi ${name}, welcome to Podify! There are so much thing that we do for verified users. Use the given OTP to verify your email.`;
+
     //ce code nous permet d'envoyer l'email de verification dans nodemailer.
     transport.sendMail({
         to: user.email,
         from: "auth@myapp.com",
-        html: `<h1>Your verification token is ${token}</h1>`,
+        html: generateTemplate({
+            title: "Welcome to Podify",
+            message: welcomeMessage,
+            logo: "cid:logo",
+            banner: "cid:welcome",
+            link: "#",
+            btnTitle: token
+        }),
+        attachments:[
+            {
+                filename: "logo.png",
+                path: path.join(__dirname, "../mail/logo.png"),
+                cid: "logo",
+            },
+            {
+                filename: "welcome.png",
+                path: path.join(__dirname, "../mail/welcome.png"),
+                cid: "welcome",
+            },
+        ]
     });
 
     res.status(201).json({ user })

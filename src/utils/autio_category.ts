e232736fb4b1f52ccee29d0913c5d13d7db65791
Lamine DIOUBATE <domanilamine96@gmail.com>
2025-03-08 14:33:59 -0500

    audio models done

---
+++ src/utils/autio_category.ts
@@ -0,0 +1,20 @@
+export const categories = [
+    "Arts",
+    "Education",
+    "Entertainment",
+    "Kids & family",
+    "Music",
+    "Science",
+    "Tech",
+    "Others",
+];
+
+export type categoriesTypes = 
+|"Arts"
+|"Education"
+|"Entertainment"
+|"Kids & family"
+|"Music"
+|"Science"
+|"Tech"
+|"Others";

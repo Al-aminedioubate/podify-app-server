0ba4b42db475cedf0f1bb43b1230cc0e4de4b4ac
Lamine DIOUBATE <domanilamine96@gmail.com>
2025-03-08 15:12:20 -0500

    audio validation and middleware done

--- src/utils/validationSchema.ts
+++ src/utils/validationSchema.ts
@@ -1,5 +1,6 @@
 import * as yup from "yup";
 import { isValidObjectId } from "mongoose";
+import { categories } from "./autio_category";
 
 export const CreateUserSchema = yup.object().shape({
     name: yup.string().trim().required("Name is missing!").min(3, "Name is too short!").max(20, "Name is too long!"),
@@ -38,4 +39,11 @@ export const UpdatePasswordSchema = yup.object().shape({
 export const SignInValidationSchema = yup.object().shape({
     email: yup.string().required("Email is missing ").email("Invalid email id!"),
     password: yup.string().trim().required("Password is missing!"),
+})
+
+//Audio validation schema
+export const AudioValidationSchema = yup.object().shape({
+    title: yup.string().required("Title is missing "),
+    about: yup.string().required("About is missing "),
+    category: yup.string().oneOf(categories, "Invalide category!").required("Category is missing!"),
 })

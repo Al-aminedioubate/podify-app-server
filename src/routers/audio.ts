4ac3019e53949a4344865bdfc5d16230f0e62919
Lamine DIOUBATE <domanilamine96@gmail.com>
2025-03-19 15:32:48 -0400

    updating audio completed

--- src/routers/audio.ts
+++ src/routers/audio.ts
@@ -1,4 +1,4 @@
-import { createAudio } from "#/controllers/audio";
+import { createAudio, updateAudio } from "#/controllers/audio";
 import { isVerified, mustAuth } from "#/middleware/auth";
 import fileParser from "#/middleware/fileParser";
 import { validate } from "#/middleware/validator";
@@ -7,6 +7,7 @@ import { Router } from "express";
 
 const router = Router();
 
-router.post("/create", mustAuth, isVerified, fileParser, validate(AudioValidationSchema), createAudio)
+router.post("/create", mustAuth, isVerified, fileParser, validate(AudioValidationSchema), createAudio);
+router.patch(":/audioId", mustAuth, isVerified, fileParser, validate(AudioValidationSchema), updateAudio);
 
 export default router
